6011235504ddec39b7cd0bb1401338bafbe79f42
Rohan Kumar Singh <158573477+Numericalalpha02@users.noreply.github.com>
2025-02-17 21:07:46 +0530

    Add files via upload

---
+++ floatnomulti.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class floatnomulti {
+    public static void main(String [] args){
+
+        float n1 = 1.2f;
+        float n2 = 4.8f;
+
+        float p = n1 * n2;
+        
+        System.out.println("The product is: " + p);
+    }
+}
